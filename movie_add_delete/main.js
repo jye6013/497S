31757a765282037f52575c81b033533e1d9506c3
huy2512hcm <bahuyhoang2512@gmail.com>
2021-10-25 22:50:04 -0400

    adding readme.md and fix route

--- movie_add_delete/main.js
+++ movie_add_delete/main.js
@@ -8,26 +8,34 @@ const port = 5000
 const movie_list = {}
 
 app.get('/movies', (req, res) => {
-    res.send(movie_list)
+    res.status(201).send(movie_list);
 })
 
-app.post('/movies/:id/update', (req, res) => {
+app.post('/movies/:id/add', (req, res) => {
     const id = req.params.id;
     const data = req.body;
     const title = data.title;
     const description = data.description;
-    movie_list[id] ={
-        title,
-        description
+    if (Object.keys(data).length === 0 || title.length === 0 || description.length === 0) {
+        res.status(400).send("Invalid data");
     }
-    res.status(201).send(movie_list);
-
+    else {
+        movie_list[id] ={
+            title,
+            description
+        }
+        res.status(201).send(movie_list);
+    }
 })
 
-app.delete('/movies/:id/delete/', (req, res) => {
+app.delete('/movies/:id/delete', (req, res) => {
     const id = req.params.id;
-    delete movie_list[id]
-    res.status(201).send(movie_list)
+    if (!(id in movie_list)) {
+        res.status(400).send("Movie ID " + id + " does not exist.");
+    } else {
+        delete movie_list[id]
+        res.status(201).send(movie_list)
+    }
 })
 app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`)
