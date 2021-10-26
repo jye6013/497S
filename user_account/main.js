bec4fb983e8bca3983f0e35c92c420f080473db3
Hung Nguyen <manhhunglvt@gmail.com>
2021-10-26 09:40:48 -0400

    user_account updates

--- user_account/main.js
+++ user_account/main.js
@@ -13,36 +13,35 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'./public')))
 
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname+'/express/login.html'));
+  res.sendFile(path.join(__dirname+'/express/login.html'))
   //__dirname : It will resolve to your project folder.
 })
 
 app.post('/register', async (req, res) => {
     try{
-        let foundUser = users.find((data) => req.body.email === data.email);
+        let foundUser = users.find((data) => req.body.email === data.email)
         if (!foundUser) {
     
-            let hashPassword = await bcrypt.hash(req.body.password, 10);
+            let hashPassword = await bcrypt.hash(req.body.password, 10)
     
             let newUser = {
                 id: Date.now(),
                 username: req.body.username,
                 email: req.body.email,
                 password: hashPassword,
-            };
-            users.push(newUser);
-            console.log('User list', users);
+            }
+            users.push(newUser)
+            console.log('User list', users)
     
-            res.send("<div align ='center'><h2>Registration successful</h2><div align='center'><a href='./registration.html'>Register another user</a></div>");
+            res.status(201).send("<div align ='center'><h2>Registration successful</h2><div align='center'><a href='./'>Register another user</a></div>")
         } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+            res.status(409).send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./'>Use a different email</a></div>") 
         }
     } catch{
-        res.send("Internal server error");
+        res.status(500).send("Internal server error")
     }
 })
 
-
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log("listening at http://localhost:${port}")
 })
