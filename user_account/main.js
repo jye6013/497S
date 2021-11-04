c15e3c7b1c32b02077aeb3484106b99d7f24f877
Hung Nguyen <manhhunglvt@gmail.com>
2021-11-04 13:41:37 -0400

    update README.md

--- user_account/main.js
+++ user_account/main.js
@@ -42,6 +42,34 @@ app.post('/register', async (req, res) => {
     }
 })
 
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email)
+        if (foundUser) {
+    
+            let submittedPass = req.body.password
+            let storedPass = foundUser.password
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass)
+            if (passwordMatch) {
+                let usrname = foundUser.username
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`)
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>")
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`
+            await bcrypt.compare(req.body.password, fakePass)
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>")
+        }
+    } catch{
+        res.send("Internal server error")
+    }
+})
+
 app.listen(port, '0.0.0.0', () => {
-    console.log("listening at http://localhost:${port}")
+    console.log(`Example app listening at http://localhost:${port}`)
 })
